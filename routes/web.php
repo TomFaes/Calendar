6e844ac081dd94e824f08bf8dcba538dce431cb5
Tom Faes <tomfa@hotmail.com>
2019-07-09 13:33:27 +0200

    v01/Tweaks: change home page

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,10 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
 */
 
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/home', '\App\Http\Controllers\Season\SeasonController@index');
+Route::get('/home', 'HomeController@index');
+Route::get('/', '\App\Http\Controllers\Season\SeasonGeneratorController@index')->name('home');
 
 /** 
  * All Auth routes 
