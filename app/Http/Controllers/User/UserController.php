c1646785f1fa9e169413334c9c7a05956a07b7cd
Tom Faes <tomfa@hotmail.com>
2019-07-15 13:16:03 +0200

    v01/Tweaks: document UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -29,34 +29,75 @@ class UserController extends Controller
         $this->user = $user;
     }
     
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index(){
         return view('user.index')->with('users', $this->user->getAllUsers());
     }
     
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create(){
         return view('user.create');
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request){
         $this->userValidation->validateCreateUser($request);
         $this->user->create($request);
         return redirect()->to('user/')->send();
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show($id){
         //
     }
 
+     /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit($id){
         return view('user.edit')->with('user', $this->user->getUser($id));
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id){
         $this->userValidation->validateCreateUser($request, $id);
         $this->user->update($request, $id);
         return redirect()->to('user/')->send();
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id){
         $this->user->delete($id);
         return redirect()->to('user/')->send();
